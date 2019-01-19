875727aa54b4e989d176398796413ce638757e47
Desalegn Berihun <desunew@gmail.com>
2019-01-19 17:17:47 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log('HELLO')
+}
+function logWhisper(string){
+  console.log("hello")
+}
+function sayHiToGrandma(string){
+  return sayHiToGrandma(string).lowercase? "I can't hear you!" : "YES INDEED!"
+    }else{
+      return "I love you, too."
+  }
+}
