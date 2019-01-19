91d6fe709a2c8539c7782043a17945f53c28ef75
Desalegn Berihun <desunew@gmail.com>
2019-01-19 17:42:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function logWhisper(string){
   console.log("hello")
 }
 function sayHiToGrandma(string){
-  var uppercase = "HELLO"
-  var lowercase = "hello"
+  var uppercase 
+  var lowercase 
   var lowercase = uppercase.toLowerCase
   var uppercase = lowercase.toUpperCase
   if (string.lowercase = true){
